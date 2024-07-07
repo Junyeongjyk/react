1dd1da0e383e6218b8d0368268978422bb354329
Junyeongjyk <kymkymrla@gmail.com>
2024-07-07 23:55:03 +0900

    모달 추가

---
+++ my-app/src/component/Modal.js
@@ -0,0 +1,26 @@
+import { useState } from "react";
+import "../assets/modal.scss";
+
+export function ModalSection() {
+  let [modal, setModal] = useState(false);
+
+  return (
+    <div className="modal-section">
+      <span>모달을 열까요?</span>
+      <button
+        onClick={() => {
+          setModal(!modal);
+        }}
+      >
+        클릭
+      </button>
+      {modal === true && <Modal />}
+    </div>
+  );
+}
+
+function Modal() {
+  return <div className="modal">이것은 모달입니다.</div>;
+}
+
+
