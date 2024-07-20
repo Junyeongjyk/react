4e59145035586dece290a6a8478d1d1637150075
Junyeongjyk <kymkymrla@gmail.com>
2024-07-20 18:06:04 +0900

    로딩 추가 컴포넌트 추가

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,17 +1,35 @@
-
+// src/App.js
 import './App.css';
-
-import {Route, Routes} from "react-router-dom";
+import './App.scss';
+import React, { useState, useEffect } from 'react';
+import LoadingSpinner from './utils/Loading'; // 로딩 스피너 컴포넌트를 불러옵니다
+import { Route, Routes } from 'react-router-dom';
 import BoardList from "./routes/BoardList";
 import Home from "./routes/Home";
-import React from "react";
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    // 3초 후에 로딩 상태를 false로 변경
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 3000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
-    <Routes>
-      <Route path="/" element={<Home/>}/>
-      <Route path="/board" element={<BoardList/>}/>
-    </Routes>
+    <div>
+      {isLoading ? (
+        <LoadingSpinner />
+      ) : (
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/board" element={<BoardList />} />
+        </Routes>
+      )}
+    </div>
   );
 }
 
--- my-app/src/utils/Loading.js
+++ my-app/src/utils/Loading.js
@@ -1,36 +1,34 @@
-import React from 'react'
-import {GridLoader} from 'react-spinners';
+// src/components/LoadingSpinner.js
+import React from 'react';
+import styled, { keyframes } from 'styled-components';
 
-//로딩 페이지 
-const override = {
-    span: '20px',
-    margin : '0 auto',
-    marginTop:'220px',
-    textAlign : 'center',
-    color : '#fff',
-    size : '20'
+const spin = keyframes`
+  0% { transform: rotate(0deg); }
+  100% { transform: rotate(360deg); }
+`;
+
+const SpinnerContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`;
+
+const Spinner = styled.div`
+  border: 16px solid #f3f3f3; /* Light grey */
+  border-top: 16px solid #3498db; /* Blue */
+  border-radius: 50%;
+  width: 120px;
+  height: 120px;
+  animation: ${spin} 2s linear infinite;
+`;
+
+const LoadingSpinner = () => {
+  return (
+    <SpinnerContainer>
+      <Spinner />
+    </SpinnerContainer>
+  );
 };
 
-const Loading = ({loading }) =>{
-    return (
-        <div>
-            <GridLoader 
-            color = "#fff"
-            loading ={loading}
-            cssOverride={override}
-            size={25}
-            speedMultiplier={0.8}
-            margin={5}
-            />
-            <div style = {{
-                padding:'20px',
-                color:'#fff',
-                fontWeight : '700',
-            }}>
-            <h> 사진을 분석하고 있어요 </h>
-            </div>
-        </div>
-    )
-}
-
-export default Loading;
+export default LoadingSpinner;
